d9e7119100f597953356d8122ca0a56ae7b71023
kishorjha12 <kishorkumarjha94@gmail.com>
2018-03-25 12:37:43 +0530

    twitter Sentiment

---
+++ credentials.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 11:02:20 2018
+
+@author: kishor
+"""
+
+#consumer key and access token
+consumer_key = '0pqFOXsLPf87ht87RZYkhUj4r'
+consumer_secret = 'wVcnL3tpXV5gJf8Tbbqje5CPHgaxzrSM4vyh9mOKw6zp8MDZMZ'
+
+
+access_token = '2300044628-Tm6fJkuzXrjtgXSlyHeNW6z6OvVGlRSynn1Mrzd'
+access_token_secret = 'QoXcWmwlCBmMgzchnjygd4vmJsAswez6j0HL6h6fmzSMf'
+
